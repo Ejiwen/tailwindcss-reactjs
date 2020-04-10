6cf55843c6a2a13a57a3e870dbe8d35cff12611e
Cheikhany ould Ejiwen <skibnewrim@gmail.com>
2020-04-09 21:40:53 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+let App = () => <div tw="bg-red text-white">hello</div>;
+
+ReactDOM.render(<App />, document.getElementById("app"));
